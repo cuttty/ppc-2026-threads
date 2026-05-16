d6c0cfca5f391972d1d53e45d7fdb624f3384b9d
cuttty <frolova.sofya.05@mail.ru>
2026-05-16 11:48:00 +0300

    fix build

--- tasks/frolova_s_radix_sort_double/all/include/ops_all.hpp
+++ tasks/frolova_s_radix_sort_double/all/include/ops_all.hpp
@@ -1,28 +1,28 @@
-s #pragma once
+#pragma once
 #include <string>
 #include <vector>
 
 #include "task/include/task.hpp"
 
-    namespace frolova_s_radix_sort_double {
+namespace frolova_s_radix_sort_double {
 
-  using InType = std::vector<double>;
-  using OutType = std::vector<double>;
-  using TestType = std::string;
-  using BaseTask = ppc::task::Task<InType, OutType>;
+using InType = std::vector<double>;
+using OutType = std::vector<double>;
+using TestType = std::string;
+using BaseTask = ppc::task::Task<InType, OutType>;
 
-  class RadixSortDouble : public BaseTask {
-   public:
-    static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
-      return ppc::task::TypeOfTask::kALL;
-    }
-    explicit RadixSortDouble(const InType &in);
+class RadixSortDouble : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kALL;
+  }
+  explicit RadixSortDouble(const InType &in);
 
-   private:
-    bool ValidationImpl() override;
-    bool PreProcessingImpl() override;
-    bool RunImpl() override;
-    bool PostProcessingImpl() override;
-  };
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+};
 
 }  // namespace frolova_s_radix_sort_double
